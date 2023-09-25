88267ca587a9fd57a09c1bfca511fb58d02bcf5e
Espilonius <435518@student.fontys.nl>
2023-09-25 21:27:40 +0200

    updating azure service

--- Colleak-Back-end/Controllers/EmployeesController.cs
+++ Colleak-Back-end/Controllers/EmployeesController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Colleak_Back_end.Controllers
 {
 
-    [Route("api/[controller]")]
+    [Route("/")]
     [ApiController]
     public class EmployeesController : ControllerBase
     {
--- Colleak-Back-end/Program.cs
+++ Colleak-Back-end/Program.cs
@@ -16,19 +16,23 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.ConfigureSwaggerGen(setup =>
+{
+    setup.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Employees controller",
+        Version = "v1"
+    });
+});
+
 builder.Services.AddScoped<IEmployeesService, EmployeesService>();  
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseSwagger();
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-if (app.Environment.IsProduction())
-{
-    app.UseSwagger();
     app.UseSwaggerUI();
 }
 
