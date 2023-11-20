2dd6f32eb2ef0f79b4594281dce02a5e51e8fb17
artofghost <67277022+Artnooijen@users.noreply.github.com>
2023-11-20 13:08:48 +0100

    updated mock api

--- MockApi/MockApi.py
+++ MockApi/MockApi.py
@@ -1,65 +1,98 @@
 from flask import Flask, request, jsonify
+import logging
 
 app = Flask(__name__)
+logging.basicConfig(level=logging.INFO)
 
-is_available = True
+# mock body for the post request
+
+# {
+#     "sender_name": "Art Nooijen",
+#     "receiver_name": "jelle Manders",
+#     "sender_id": "653fad3009ae93a5292195d4",
+#     "receiver_id": "653fb6cb09ae93a5292195d6",
+#     "message": "Hello, When do you have time to meet?",
+#     "request_time": "11",
+#     "disturb": "true"
+# }
+
+def validate_input(data, required_fields):
+    missing_fields = [field for field in required_fields if field not in data]
+    if missing_fields:
+        return False, f"Missing fields: {', '.join(missing_fields)}"
+    return True, ""
+
 @app.route('/send_message', methods=['POST'])
 def send_message():
     data = request.json
-    sender_id = data.get('sender_id')
-    receiver_id = data.get('receiver_id')
-    message = data.get('message')
-    sender_name = data.get('sender_name')
-    receiver_name = data.get('receiver_name')
+    valid, error_message = validate_input(data, ['sender_id', 'receiver_id', 'message', 'sender_name', 'receiver_name'])
+    if not valid:
+        return jsonify({"status": "error", "message": error_message}), 400
 
-    # {
-    # "sender_name": "Art",
-    # "reciever_name": "jelle",
-    # "sender_id": "1",
-    # "receiver_id": "2",
-    # "message": "Hello, When do you have time to meet?"
-    # }
-    print(f"Message from {sender_name}({sender_id}) to {receiver_name}({receiver_id}): {message}")
+    sender_id = data['sender_id']
+    receiver_id = data['receiver_id']
+    message = data['message']
+    sender_name = data['sender_name']
+    receiver_name = data['receiver_name']
 
-    return jsonify({"status": "success", "message": "Message sent successfully"})
+    logging.info(f"Message from {sender_name}({sender_id}) to {receiver_name}({receiver_id}): {message}")
+    return jsonify({"status": "success", "message": message}),200
 
 @app.route('/send_ping', methods=['POST'])
-def Send_ping():
+def send_ping():
     data = request.json
-    sender_id = data.get('sender_id')
-    receiver_id = data.get('receiver_id')
+    valid, error_message = validate_input(data, ['sender_id', 'receiver_id', 'sender_name', 'receiver_name'])
+    if not valid:
+        return jsonify({"status": "error", "message": error_message}), 400
+
+    sender_id = data['sender_id']
+    receiver_id = data['receiver_id']
     message = 'is looking for you!'
-    sender_name = data.get('sender_name')
-    receiver_name = data.get('receiver_name')
+    sender_name = data['sender_name']
+    receiver_name = data['receiver_name']
 
-
-    print(f"{sender_name}({sender_id}) {message} Send to: {receiver_name}({receiver_id})")
-
+    logging.info(f"{sender_name}({sender_id}) {message} Send to: {receiver_name}({receiver_id})")
     return jsonify({"status": "success", "message": "User pinged successfully"})
-
-
-Not_Available_Array = [11,12,13]
-
+Not_Available_Array = [11, 12, 13, 18, 19]
 @app.route('/available', methods=['POST'])
 def is_person_available():
     data = request.json
-    sender_id = data.get('sender_id')
-    request_time = int(data.get('request_time'))  # Convert request_time to integer
-    receiver_id = data.get('receiver_id')
-    sender_name = data.get('sender_name')
-    receiver_name = data.get('receiver_name')
+    valid, error_message = validate_input(data, ['sender_id', 'receiver_id', 'sender_name', 'receiver_name', 'request_time'])
+    if not valid:
+        return jsonify({"status": "error", "message": error_message}), 400
 
-    is_available = True  # Initialize is_available to True
+    sender_id = data['sender_id']
+    receiver_id = data['receiver_id']
+    request_time = int(data['request_time'])
+    sender_name = data['sender_name']
+    receiver_name = data['receiver_name']
 
-    if request_time in Not_Available_Array:
-        is_available = False
+    is_available = request_time not in Not_Available_Array
 
     if is_available:
-        print(f"{sender_name}({sender_id}) is available. Send to: {receiver_name}({receiver_id}) at {request_time}")
+        logging.info(f"{sender_name}({sender_id}) is available. Send to: {receiver_name}({receiver_id}) at {request_time}")
         return jsonify({"status": "success", "message": "User is available"})
     else:
-        print(f"{receiver_name}({receiver_id}) is not available at {request_time}")
-        return jsonify({"status": "error", "message": "User is not available"})
+        logging.info(f"{receiver_name}({receiver_id}) is not available at {request_time}")
+        return jsonify({"status": "success", "message": "User is not available"})
+
+@app.route('/disturb', methods=['POST'])
+def disturb():
+    data = request.json
+    valid, error_message = validate_input(data, ['receiver_id', 'receiver_name', 'disturb'])
+    if not valid:
+        return jsonify({"status": "error", "message": error_message}), 400
+
+    receiver_id = data['receiver_id']
+    receiver_name = data['receiver_name']
+    disturb = data['disturb'].lower() == 'true'
+
+    if disturb:
+        logging.info(f"{receiver_name}({receiver_id}) can be contacted")
+        return jsonify({"status": "success", "message": "You are able to be contacted"})
+    else:
+        logging.info(f"{receiver_name}({receiver_id}) cannot be contacted")
+        return jsonify({"status": "success", "message": "Your location is no longer shared"})
 
 if __name__ == '__main__':
     app.run(debug=True, port=8001)
